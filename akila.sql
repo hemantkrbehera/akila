5e35bee8a0fe0622e672656532c640c4a7a01eea
hemant <>
2019-11-12 15:46:44 +0530

    this is edited to sales

--- akila.sql
+++ akila.sql
@@ -1,1 +1,1 @@
-select * FRom employees where department_id=(Select department_id from departments where department_name='IT');
+select * FRom employees where department_id=(Select department_id from departments where department_name='Sales');
